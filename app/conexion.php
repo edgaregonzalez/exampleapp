bdec2ea1678f01b1bb35065d7e0a3dd7392ae9f5
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2017-09-10 19:16:53 -0300

    Modificacion de nombre db

--- app/conexion.php
+++ app/conexion.php
@@ -6,12 +6,12 @@ function Connect()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $dbname = "responses";
+ $dbname = "mensajesdb";
 
  // Crear una conexion
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
 
  return $conn;
 }
- 
+
 ?>
