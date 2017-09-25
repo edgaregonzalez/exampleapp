b37716598188882321170efa6e5e33a9b6d12dc4
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2017-09-25 10:38:21 -0300

    Update conexion.php
    Modificación para que pueda recibir variable de entorno desde docker stack.

--- app/conexion.php
+++ app/conexion.php
@@ -3,10 +3,10 @@
 
 function Connect()
 {
- $dbhost = "";
- $dbuser = "";
- $dbpass = "";
- $dbname = "";
+ $dbhost = getenv("DB_HOST");
+ $dbuser = getenv("DB_USER");
+ $dbpass = getenv("DB_PASS");
+ $dbname = getenv("DB_NAME");
 
  // Crear una conexion
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
